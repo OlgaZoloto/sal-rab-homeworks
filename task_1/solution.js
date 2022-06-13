d8178a78597f9237bead18f2114a061ad51fc5b8
Olga Zolotova <vp123451234@gmail.com>
2022-06-13 21:39:09 +0300

    Задание 1

--- task_1/solution.js
+++ task_1/solution.js
@@ -12,8 +12,11 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message значение переменной productPrice
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    let message = productName + " за " + productPrice + " теперь в корзине!";
+
     // выведите переменную message в консоль
 
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
